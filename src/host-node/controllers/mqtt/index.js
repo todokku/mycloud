e2d9785c9116da916c3500b7f3100f8da449988f
Michael Dundek <mdundek@gmail.com>
2020-04-06 12:17:52 +0200

    debugging

--- src/host-node/controllers/mqtt/index.js
+++ src/host-node/controllers/mqtt/index.js
@@ -67,6 +67,7 @@ class MqttController {
                
                 if(topic.startsWith("/mycloud/k8s/host/query/k8s_nodes/free_memory")){
                     let freeMem = await OsController.getFreeMemory();
+                    console.log("freeMem =>", `/mycloud/k8s/host/respond/${data.queryTarget}/${topicSplit[5]}/${topicSplit[6]}`, freeMem);
                     let data = JSON.parse(message.toString());
                     this.client.publish(`/mycloud/k8s/host/respond/${data.queryTarget}/${topicSplit[5]}/${topicSplit[6]}`, JSON.stringify({
                         status: 200,
