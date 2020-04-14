ece7596aaf22f15d0378749d4d03ad777221d846
Michael Dundek <mdundek@gmail.com>
2020-04-14 22:17:38 +0200

    debugging

--- src/host-node/resources/scripts/bootstrap_k8s/bootstrap_master.sh
+++ src/host-node/resources/scripts/bootstrap_k8s/bootstrap_master.sh
@@ -47,7 +47,8 @@ sed -i '/- kube-apiserver/a\ \ \ \ - --runtime-config=settings.k8s.io/v1alpha1=t
 
 # Install Gluster client
 echo "[TASK M.4] Install Gluster engine"
-yum install -y -q centos-release-gluster glusterfs-server
+yum install -y -q centos-release-gluster
+yum install -y -q glusterfs-server
 systemctl disable glusterd
 systemctl stop glusterd
 
--- src/host-node/resources/scripts/bootstrap_k8s/bootstrap_worker.sh
+++ src/host-node/resources/scripts/bootstrap_k8s/bootstrap_worker.sh
@@ -2,8 +2,7 @@
 
 # Install Gluster client
 echo "[TASK M.4] Install Gluster engine"
-yum install -y -q centos-release-gluster glusterfs-server
-echo "[TASK M.5]"
+yum install -y -q centos-release-gluster
+yum install -y -q glusterfs-server
 systemctl disable glusterd
-echo "[TASK M.6]"
 systemctl stop glusterd
