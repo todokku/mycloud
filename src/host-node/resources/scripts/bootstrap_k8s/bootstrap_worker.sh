5bdd470ef17b2a85f4b2e1ea183cfd2321c2dcff
Michael Dundek <mdundek@gmail.com>
2020-04-14 21:33:06 +0200

    debugging

--- src/host-node/resources/scripts/bootstrap_k8s/bootstrap_worker.sh
+++ src/host-node/resources/scripts/bootstrap_k8s/bootstrap_worker.sh
@@ -3,5 +3,7 @@
 # Install Gluster client
 echo "[TASK M.4] Install Gluster engine"
 yum install -y -q centos-release-gluster glusterfs-server
+echo "[TASK M.5]"
 systemctl disable glusterd
+echo "[TASK M.6]"
 systemctl stop glusterd
