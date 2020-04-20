7e9714f5b83e2e7a9d26d81350b79f43e367cf50
Michael Dundek <mdundek@gmail.com>
2020-04-20 23:22:10 +0200

    debugging

--- src/api/src/services/organizations/organizations.hooks.js
+++ src/api/src/services/organizations/organizations.hooks.js
@@ -81,23 +81,25 @@ module.exports = {
 		all: [],
 		find: [
 			async context => {
+				console.log(1);
 				if(await Permissions.isSysAdmin(context) || context.params._internalRequest){
 					delete context.params._internalRequest;
 					return context;
 				}
-
+				console.log(1);
 				let userId = Permissions.getUserIdFromJwt(context.params.authentication.accessToken);
 				let orgUsers = await context.app.service('org-users').find({
 					query: {
 						userId: userId
 					}
 				});
-		
+				console.log(1);
 				// Itterate over all returned organizations
 				context.result.data = context.result.data.filter((org) => {
 					return orgUsers.find(o => o.organizationId == org.id) ? true : false;
 					
 				});
+				console.log(1);
 				context.result.total = context.result.data.length;
 				return context;
 			}
