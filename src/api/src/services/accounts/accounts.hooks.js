422333b4f24802c238a5bfdfc22beb61588e4464
Michael Dundek <mdundek@gmail.com>
2020-04-20 18:48:02 +0200

    debugging

--- src/api/src/services/accounts/accounts.hooks.js
+++ src/api/src/services/accounts/accounts.hooks.js
@@ -65,7 +65,28 @@ module.exports = {
 
 	after: {
 		all: [],
-		find: [],
+		find: [async context => {
+			// Is user is sysadmin, return it all
+			if(await Permissions.isSysAdmin(context) || context.params._internalRequest){
+				delete context.params._internalRequest;
+				return context;
+			}
+
+			let accUsers = await context.app.service('acc-users').find({
+				query: {
+					userId: context.params.user.id
+				},
+				user: context.params.user,
+				_internalRequest: true
+			});
+			
+			// Itterate over all returned organizations
+			context.result.data = context.result.data.filter((acc, z) => {
+				return accUsers.find(o => o.accountId == acc.accountId);
+			});
+			context.result.total = context.result.data.length;
+			return context;
+		}],
 		get: [],
 		create: [],
 		update: [],
