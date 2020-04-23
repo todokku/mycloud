f905c9664c782725136cb3a28ceb9b843abbfe1c
Michael Dundek <mdundek@gmail.com>
2020-04-23 15:47:47 +0200

    debugging

--- src/api/src/services/org-users/org-users.hooks.js
+++ src/api/src/services/org-users/org-users.hooks.js
@@ -25,33 +25,33 @@ module.exports = {
   after: {
     all: [],
     find: [
-      //   async context => {
-      //   if(await Permissions.isSysAdmin(context) || context.params._internalRequest){
-      //     delete context.params._internalRequest;
-      //     return context;
-      //   }
+      async context => {
+        if(await Permissions.isSysAdmin(context) || context.params._internalRequest){
+          delete context.params._internalRequest;
+          return context;
+        }
+        console.log(context);
+        let userId = Permissions.getUserIdFromJwt(context.params.authentication.accessToken);
+        let orgUsers = await context.app.service('org-users').find({
+          paginate: false,
+          query: {
+            userId: userId
+          }
+        });
         
-      //   let userId = Permissions.getUserIdFromJwt(context.params.authentication.accessToken);
-      //   let orgUsers = await context.app.service('org-users').find({
-      //     paginate: false,
-      //     query: {
-      //       userId: userId
-      //     }
-      //   });
-        
-      //   // Itterate over all returned organizations
-      //   if(context.result.data){
-      //     context.result.data = context.result.data.filter((orgUser) => {
-      //       return orgUsers.find(o => o.organizationId == orgUser.organizationId) ? true : false;
-      //     });
-      //     context.result.total = context.result.data.length;
-      //   } else {
-      //     context.result = context.result.filter((orgUser) => {
-      //       return orgUsers.find(o => o.organizationId == orgUser.organizationId) ? true : false;
-      //     });
-      //   }
-      //   return context;
-      // }
+        // Itterate over all returned organizations
+        if(context.result.data){
+          context.result.data = context.result.data.filter((orgUser) => {
+            return orgUsers.find(o => o.organizationId == orgUser.organizationId) ? true : false;
+          });
+          context.result.total = context.result.data.length;
+        } else {
+          context.result = context.result.filter((orgUser) => {
+            return orgUsers.find(o => o.organizationId == orgUser.organizationId) ? true : false;
+          });
+        }
+        return context;
+      }
     ],
     get: [],
     create: [],
