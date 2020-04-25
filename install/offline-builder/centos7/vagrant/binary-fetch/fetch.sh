ba85e26daf5a87c58d173e9d1462cc28a095bca4
Michael Dundek <mdundek@gmail.com>
2020-04-25 16:53:29 +0200

    debugging

--- install/offline-builder/centos7/vagrant/binary-fetch/fetch.sh
+++ install/offline-builder/centos7/vagrant/binary-fetch/fetch.sh
@@ -20,17 +20,27 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 yum update
 
 # ********** FETCH REQUIRED RMP PACKAGES ************
-yum install -y yum-utils wget
-yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+# yum install -y yum-utils wget
+# yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
-mkdir /var/tmp/rpms/epel-release-latest-7
-wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -O /var/tmp/rpms/epel-release-latest-7/epel-release-latest-7.noarch.rpm
+# mkdir /var/tmp/rpms/epel-release-latest-7
+# wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -O /var/tmp/rpms/epel-release-latest-7/epel-release-latest-7.noarch.rpm
 
-mkdir /var/tmp/rpms/virtualbox
-wget https://download.virtualbox.org/virtualbox/6.1.4/VirtualBox-6.1-6.1.4_136177_el7-1.x86_64.rpm -O /var/tmp/rpms/virtualbox/VirtualBox-6.1-6.1.4_136177_el7-1.x86_64.rpm
+# mkdir /var/tmp/rpms/virtualbox
+# wget https://download.virtualbox.org/virtualbox/6.1.4/VirtualBox-6.1-6.1.4_136177_el7-1.x86_64.rpm -O /var/tmp/rpms/virtualbox/VirtualBox-6.1-6.1.4_136177_el7-1.x86_64.rpm
 
-mkdir /var/tmp/rpms/vagrant
-wget https://releases.hashicorp.com/vagrant/2.2.7/vagrant_2.2.7_x86_64.rpm -O /var/tmp/rpms/vagrant/vagrant_2.2.7_x86_64.rpm
+# mkdir /var/tmp/rpms/vagrant
+# wget https://releases.hashicorp.com/vagrant/2.2.7/vagrant_2.2.7_x86_64.rpm -O /var/tmp/rpms/vagrant/vagrant_2.2.7_x86_64.rpm
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
 
 IFS=$'\r\n' GLOBIGNORE='*' command eval  'RPM_LIST=($(cat /var/tmp/rpms/rpm-list.cfg))'
 for PACKAGE in "${RPM_LIST[@]}"; do :
@@ -42,9 +52,9 @@ for PACKAGE in "${RPM_LIST[@]}"; do :
 done
 
 # ********** FETCH REQUIRED DOCKER CONTAINERS ************
-yum install -y docker-ce
-systemctl enable docker
-systemctl start docker
+# yum install -y docker-ce
+# systemctl enable docker
+# systemctl start docker
 
 IFS=$'\r\n' GLOBIGNORE='*' command eval  'DIMG_LIST=($(cat /var/tmp/docker-images/image-list.cfg))'
 for PACKAGE in "${RPM_LIST[@]}"; do :
@@ -54,7 +64,7 @@ for PACKAGE in "${RPM_LIST[@]}"; do :
         D_IMG=$(echo $PACKAGE | cut -d' ' -f1)
         D_VER=$(echo $PACKAGE | cut -d' ' -f2)
         F_NAME=$(echo $PACKAGE | cut -d' ' -f3)
-        fetch_docker_images $D_IMG $D_VER $F_NAME
+        # fetch_docker_images $D_IMG $D_VER $F_NAME
     fi
 done
 
